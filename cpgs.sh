a9ceda89dcd2c9ce983691bf94390a84e3ad1ba3
Felix Weißberg <f.weissberg@tu-braunschweig.de>
2022-06-29 23:54:06 +0200

    Bugfix: global arg first

--- cpgs.sh
+++ cpgs.sh
@@ -3,6 +3,6 @@
 input_dir=$1
 outpur_dir=$2
 
-for input_file in $(find $input_dir -type f -maxdepth 1); do
+for input_file in $(find $input_dir -maxdepth 1 -type f); do
 	./cpg.sh $input_file $outpur_dir
 done

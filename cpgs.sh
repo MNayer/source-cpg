63153690f3be7d7702d24815506163a98cd574ea
Felix Weißberg <f.weissberg@tu-braunschweig.de>
2022-06-30 10:46:18 +0200

    Feature: Show process

--- cpgs.sh
+++ cpgs.sh
@@ -2,7 +2,11 @@
 
 input_dir=$1
 outpur_dir=$2
+total=$(find $input_dir -maxdepth 1 -type f | wc -l)
+current=0
 
 for input_file in $(find $input_dir -maxdepth 1 -type f); do
+	current=$(( $current + 1 ))
+	echo "Process file $current / $total"
 	./cpg.sh $input_file $outpur_dir
 done
